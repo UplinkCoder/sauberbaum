b757565e65818cf442d369218e31303eba2b67b8
Stefan Koch <UplinkCoder@users.noreply.github.com>
2016-10-03 21:13:59 +0200

    Create sauberbaum.d

---
+++ sauberbaum.d
@@ -0,0 +1,25 @@
+module sauerbaum;
+
+auto sauerbaum_layout = parseSauberBaum(	   
+`      x
+	 x x  
+	x x x
+   x x x x   
+  x x x x x 
+   x x x x
+    x x x
+   x x x x
+  x x x x x
+ x x x x x x   
+x x x o x x x
+      x	   
+      x
+x x x ^ x x x`
+);
+
+
+auto parseSauberBaum (string sbl)
+{
+    foreach(sbl.lines);
+}
+ 
